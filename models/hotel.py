96133376575de24c7ae2cb9c8b8eddeb7ea83332
xugolini <fabianougolini@hotmail.com>
2023-09-27 15:30:18 -0300

    adicionando banco sqlite utilizando SQLAlchemy

--- models/hotel.py
+++ models/hotel.py
@@ -1,4 +1,14 @@
-class HotelModel:
+from sql_alchemy import banco
+
+class HotelModel(banco.Model):
+    __tablename__ = 'hoteis'
+
+    hotel_id = banco.Column(banco.String, primary_key=True)
+    nome = banco.Column(banco.String(80))
+    estrelas = banco.Column(banco.Float(precision=1))
+    diaria = banco.Column(banco.Float(precision=2))
+    cidade = banco.Column(banco.String(40))
+
     def __init__(self, hotel_id, nome, estrelas, diaria, cidade):
         self.hotel_id = hotel_id
         self.nome = nome
@@ -14,3 +24,14 @@ class HotelModel:
             'diaria': self.diaria,
             'cidade': self.cidade
         }
+    
+    @classmethod
+    def find_hotel(cls, hotel_id):
+        hotel = cls.query.filter_by(hotel_id = hotel_id).first()
+        if hotel:
+            return hotel
+        return None
+    
+    def save_hotel(self):
+        banco.session.add(self)
+        banco.session.commit()
--- resources/hotel.py
+++ resources/hotel.py
@@ -1,30 +1,5 @@
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
-
-hoteis = [
-    {
-        'hotel_id': 'alpha',
-        'nome': 'Alpha Hotel',
-        'estrelas': 4.3,
-        'diaria': 420.34,
-        'cidade': 'Rio de Janeiro'
-    },
-    {
-        'hotel_id': 'bravo',
-        'nome': 'Bravo Hotel',
-        'estrelas': 4.4,
-        'diaria': 380.90,
-        'cidade': 'Santa Catarina'
-    },
-    {
-        'hotel_id': 'charlie',
-        'nome': 'Charlie Hotel',
-        'estrelas': 3.9,
-        'diaria': 320.20,
-        'cidade': 'Santa Catarina'
-    }
-]     
-
 class Hoteis(Resource):
     def get(self):
         return {'hoteis': hoteis}
@@ -37,13 +12,6 @@ class Hotel(Resource):
     argumentos.add_argument('diaria')
     argumentos.add_argument('cidade')
 
-
-    def find_hotel(hotel_id):
-        for hotel in hoteis:
-            if hotel['hotel_id'] == hotel_id:
-                return hotel
-        return None
-            
     def get(self, hotel_id):
         hotel = Hotel.find_hotel(hotel_id)
 
@@ -52,12 +20,12 @@ class Hotel(Resource):
         return {'message': 'Hotel não encontrado.'}, 404
 
     def post(self, hotel_id):
+        if HotelModel.find_hotel(hotel_id):
+            return {"message": "Hotel id '{}' já existe.".format(hotel_id)}, 400
         dados = Hotel.argumentos.parse_args()
-        hotel_objeto = HotelModel(hotel_id, **dados)
-        novo_hotel = hotel_objeto.json()
-        hoteis.append(novo_hotel)
-
-        return novo_hotel, 200
+        hotel = HotelModel(hotel_id, **dados)
+        hotel.save_hotel()
+        return hotel.json()
 
     def put(self, hotel_id):
         dados = Hotel.argumentos.parse_args()
